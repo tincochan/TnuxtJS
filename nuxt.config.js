0dc995c47e3fcfe8f1b0e5274077ce472b00db95
Tinc0ist <34474781+tincochan@users.noreply.github.com>
2022-07-06 09:19:33 +0800

    SEO description

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,12 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'my website description'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
